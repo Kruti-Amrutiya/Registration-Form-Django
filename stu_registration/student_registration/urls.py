7d11790a0bf6bd7762bf98f86a2d8814d30c6dcc
Kruti Amrutiya <krutiamrutiya1998@gmail.com>
2021-02-12 17:26:03 +0530

    insert, update and delete student using class based view

--- stu_registration/student_registration/urls.py
+++ stu_registration/student_registration/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.student_form, name='student_insert'),
-    path('list/', views.student_list, name='student_list'),
-    path('<int:id>/', views.student_update, name='student_update'),
-    path('delete/<int:id>/', views.student_delete, name='student_delete'),
+    # urls for class based view
+    path('', views.StudentFormView.as_view(), name='student_insert'),
+    path('list/', views.StudentListView.as_view(), name='student_list'),
+    path('<int:id>/', views.StudentUpdateView.as_view(), name='student_update'),
+    path('delete/<int:id>', views.StudentDeleteView.as_view(), name='student_delete'),
+    # urls for function based view
+    # path('', views.student_form, name='student_insert'),
+    # path('list/', views.student_list, name='student_list'),
+    # path('<int:id>/', views.student_update, name='student_update'),
+    # path('delete/<int:id>/', views.student_delete, name='student_delete'),
 ]
